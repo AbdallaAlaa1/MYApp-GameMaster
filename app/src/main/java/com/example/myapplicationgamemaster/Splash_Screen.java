89423f4c3c513e2bce84083a9ce55be4242d6325
ABood <tymersy@gmail.com>
2021-12-16 21:08:13 +0200

    GameMaster

---
+++ app/src/main/java/com/example/myapplicationgamemaster/Splash_Screen.java
@@ -0,0 +1,31 @@
+package com.example.myapplicationgamemaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class Splash_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        getSupportActionBar().hide();
+
+        Thread thread = new Thread(){
+            public void run() {
+                try {
+                    sleep(3000);
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        thread.start();
+    }
+}
+
